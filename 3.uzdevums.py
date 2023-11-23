e8d9fd6afc0645ae8e80b36328f9ed200bca7aad
AlekssBelakovs <alekss.belakovs@grobinasskola.lv>
2023-11-23 11:47:10 +0200

    3.uzdevums

---
+++ 3.uzdevums.py
@@ -0,0 +1,6 @@
+
+ievaditais_skaitlis = int(input("Ievadiet skaitli: "))
+if ievaditais_skaitlis % 2 != 0:
+    print(ievaditais_skaitlis, "ir nepāra skaitlis.")
+else:
+    print(ievaditais_skaitlis, "nav nepāra skaitlis.")
